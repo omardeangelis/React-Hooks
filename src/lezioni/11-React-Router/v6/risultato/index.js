bd44eac3f121b16e939edfa2c48e0c44cf8120da
Omar De Angelis <omar.hpvfilm@gmail.com>
2022-04-06 22:33:42 +0200

    rrv6: Error Page & Link component

--- src/lezioni/11-React-Router/v6/risultato/index.js
+++ src/lezioni/11-React-Router/v6/risultato/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "./screen/About";
+import ErrorPage from "./screen/ErrorPage";
 import Home from "./screen/Home";
 import Profile from "./screen/Profile";
 // Non si importa più switch
@@ -8,10 +9,17 @@ import Profile from "./screen/Profile";
 const Component = () => {
   return (
     <Router>
+      <nav className='d-flex flex-row justify-content-center mx-auto gap-2 mb-4'>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/profile'>Profile</Link>
+        <Link to='/profile/1'>Profile1</Link>
+      </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </Router>
   );
---
+++ src/lezioni/11-React-Router/v6/risultato/screen/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return <div>Page Not Found</div>;
+};
+
+export default ErrorPage;
