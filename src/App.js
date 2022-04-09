2bea9e71b45c53edfac4ce4bcfa83bec1b2c7421
Omar De Angelis <omar.hpvfilm@gmail.com>
2022-04-09 16:52:20 +0200

    chore: create Rouct router v5 e v6 folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import Inizio from "./lezioni/11-React-Router/v6/risultato";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import Inizio from "./lezioni/1-useState/inizio/1-errore-base";
 
 function App() {
   return (
     <div className='App'>
       <section className='container text-center my-3'>
-        <Router>
-          <nav className='d-flex flex-row justify-content-center mx-auto gap-2 mb-4'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/profile'>Profile</Link>
-          </nav>
-          <Inizio />
-        </Router>
+        <Inizio />
       </section>
     </div>
   );
